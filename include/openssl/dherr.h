c3179235fc2d9bf1b2ff6bc6d9537786c62b8c7a
Richard Levitte <levitte@openssl.org>
2023-10-20 09:18:19 +0200

    Make DH_check_pub_key() and DH_generate_key() safer yet
    We already check for an excessively large P in DH_generate_key(), but not in
    DH_check_pub_key(), and none of them check for an excessively large Q.
    
    This change adds all the missing excessive size checks of P and Q.
    
    It's to be noted that behaviours surrounding excessively sized P and Q
    differ.  DH_check() raises an error on the excessively sized P, but only
    sets a flag for the excessively sized Q.  This behaviour is mimicked in
    DH_check_pub_key().
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Hugo Landau <hlandau@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22518)
    
    (cherry picked from commit ddeb4b6c6d527e54ce9a99cba785c0f7776e54b6)
    (cherry picked from commit db925ae2e65d0d925adef429afc37f75bd1c2017)
    
    Gbp-Pq: Name CVE-2023-5678.patch

--- include/openssl/dherr.h
+++ include/openssl/dherr.h
@@ -34,6 +34,7 @@ int ERR_load_DH_strings(void);
 #  define DH_F_DH_CHECK_EX                                 121
 #  define DH_F_DH_CHECK_PARAMS_EX                          122
 #  define DH_F_DH_CHECK_PUB_KEY_EX                         123
+#  define DH_F_DH_CHECK_PUB_KEY                            124
 #  define DH_F_DH_CMS_DECRYPT                              114
 #  define DH_F_DH_CMS_SET_PEERKEY                          115
 #  define DH_F_DH_CMS_SET_SHARED_INFO                      116
@@ -82,6 +83,7 @@ int ERR_load_DH_strings(void);
 #  define DH_R_NO_PRIVATE_VALUE                            100
 #  define DH_R_PARAMETER_ENCODING_ERROR                    105
 #  define DH_R_PEER_KEY_ERROR                              111
+#  define DH_R_Q_TOO_LARGE                                 130
 #  define DH_R_SHARED_INFO_ERROR                           113
 #  define DH_R_UNABLE_TO_CHECK_GENERATOR                   121
 
