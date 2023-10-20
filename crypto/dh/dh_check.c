e31fb973c6aa7993335a96b7d41fed2f788d7413
Richard Levitte <levitte@openssl.org>
2023-10-20 09:18:19 +0200

    Make DH_check_pub_key() and DH_generate_key() safer yet
    We already check for an excessively large P in DH_generate_key(), but not in
    DH_check_pub_key(), and none of them check for an excessively large Q.
    
    This change adds all the missing excessive size checks of P and Q.
    
    It's to be noted that behaviours surrounding excessively sized P and Q
    differ.  DH_check() raises an error on the excessively sized P, but only
    sets a flag for the excessively sized Q.  This behaviour is mimicked in
    DH_check_pub_key().
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Hugo Landau <hlandau@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22518)
    
    (cherry picked from commit ddeb4b6c6d527e54ce9a99cba785c0f7776e54b6)
    (cherry picked from commit db925ae2e65d0d925adef429afc37f75bd1c2017)
    
    Gbp-Pq: Name CVE-2023-5678.patch

--- crypto/dh/dh_check.c
+++ crypto/dh/dh_check.c
@@ -197,6 +197,18 @@ int DH_check_pub_key(const DH *dh, const BIGNUM *pub_key, int *ret)
     BIGNUM *tmp = NULL;
     BN_CTX *ctx = NULL;
 
+    /* Don't do any checks at all with an excessively large modulus */
+    if (BN_num_bits(dh->p) > OPENSSL_DH_MAX_MODULUS_BITS) {
+        DHerr(DH_F_DH_CHECK_PUB_KEY, DH_R_MODULUS_TOO_LARGE);
+        *ret = DH_MODULUS_TOO_LARGE | DH_CHECK_PUBKEY_INVALID;
+        return 0;
+    }
+
+    if (dh->q != NULL && BN_ucmp(dh->p, dh->q) < 0) {
+        *ret |= DH_CHECK_INVALID_Q_VALUE | DH_CHECK_PUBKEY_INVALID;
+        return 1;
+    }
+
     *ret = 0;
     ctx = BN_CTX_new();
     if (ctx == NULL)
