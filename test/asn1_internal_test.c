c6854917c2c5135d60da248651c5e92d8cf54ec9
Norbert Pocs <norbertp@openssl.org>
2025-12-11 12:38:16 +0100

    Harden ASN1_mbstring_ncopy
    Reported by Murali Aniruddhan
    
    Signed-off-by: Norbert Pocs <norbertp@openssl.org>
    
    Reviewed-by: Nikola Pajkovsky <nikolap@openssl.org>
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/29376)
    
    Gbp-Pq: Name Harden-ASN1_mbstring_ncopy.patch

--- test/asn1_internal_test.c
+++ test/asn1_internal_test.c
@@ -133,10 +133,27 @@ static int test_nid2obj_nonexist(void)
     return 1;
 }
 
+static int test_mbstring_ncopy(void)
+{
+    ASN1_STRING *str = NULL;
+    const unsigned char in[] = { 0xFF, 0xFE, 0xFF, 0xFE };
+    int inlen = 4;
+    int inform = MBSTRING_UNIV;
+
+    if (!TEST_int_eq(ASN1_mbstring_ncopy(&str, in, inlen, inform, B_ASN1_GENERALSTRING, 0, 0), -1)
+        || !TEST_int_eq(ASN1_mbstring_ncopy(&str, in, inlen, inform, B_ASN1_VISIBLESTRING, 0, 0), -1)
+        || !TEST_int_eq(ASN1_mbstring_ncopy(&str, in, inlen, inform, B_ASN1_VIDEOTEXSTRING, 0, 0), -1)
+        || !TEST_int_eq(ASN1_mbstring_ncopy(&str, in, inlen, inform, B_ASN1_GENERALIZEDTIME, 0, 0), -1))
+        return 0;
+
+    return 1;
+}
+
 int setup_tests(void)
 {
     ADD_TEST(test_tbl_standard);
     ADD_TEST(test_standard_methods);
     ADD_TEST(test_nid2obj_nonexist);
+    /*ADD_TEST(test_mbstring_ncopy);*/
     return 1;
 }
